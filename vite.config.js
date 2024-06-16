dc0c63fb466d2be907f860af7a344bb91565a6cb
Rustam Yusupov <i@rstm.me>
2024-06-16 14:53:48 +0200

    Update vite.config.js to remove Vite module loader

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile({ removeViteModuleLoader: true })],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
