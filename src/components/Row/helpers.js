4edbd9054075db65d52a5fbdffdb9bd190e9a2c2
Rustam Yusupov <i@rstm.me>
2024-06-16 14:26:56 +0200

    Improve formatTime function in Row component

--- src/components/Row/helpers.js
+++ src/components/Row/helpers.js
@@ -1,1 +1,10 @@
-export const formatTime = ms => ms;
+export const formatTime = ms => {
+  const hours = Math.floor(ms / 3600000);
+  const minutes = Math.floor((ms % 3600000) / 60000);
+  const seconds = Math.floor((ms % 60000) / 1000);
+  const milliseconds = Math.floor((ms % 1000) / 100);
+
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}.${milliseconds}`;
+};
