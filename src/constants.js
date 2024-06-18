5f02f99788c79dfe6ea3cba9f7993ef305e16c83
Rustam Yusupov <i@rstm.me>
2024-06-18 08:59:19 +0200

    Add constants for time and distance units

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const hundredthOfMilliseconds = 10;
+export const tenthOfMilliseconds = 100;
+export const millisecondsInSecond = 1000;
+export const millisecondsInMinute = 60000;
+export const millisecondsInHour = 3600000;
+
+export const metersInKilometer = 1000;
